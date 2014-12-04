b2d90bb4f33bb0f48438cf7f0193dade18c8b758
Alvaro <varofv66@gmail.com>
2014-12-04 13:38:22 +0100

    añadido metodo responder preguntas

--- Bebedor.java
+++ Bebedor.java
@@ -8,15 +8,24 @@
 public class Bebedor
 {
     // instance variables - replace the example below with your own
+    private String nombreBebedor;
+    
     private int cantidadAlcoholSangre;
+    
+    private int nivelMaximo;
+    
+    private String pregunta;
 
     /**
      * Constructor for objects of class Bebedor
      */
-    public Bebedor()
+    public Bebedor(String nombreBebedor)
     {
         // initialise instance variables
+        this.nombreBebedor = nombreBebedor;
         this.cantidadAlcoholSangre = 0;
+        this.nivelMaximo = 100;
+        this.pregunta = null;
     }
 
     /**
@@ -27,11 +36,40 @@ public class Bebedor
      */
     public void beberCopa(Cubata nombre)
     {
-       cantidadAlcoholSangre = cantidadAlcoholSangre + nombre.getAlcohol();
+       int cantidadAlcohol = cantidadAlcoholSangre + nombre.getAlcohol();
+       if(cantidadAlcohol > nivelMaximo)
+       {
+            System.out.print("No puedo beber más, gracias");
+           
+       }
+       else
+       {
+           cantidadAlcoholSangre = cantidadAlcohol ;
+       }
     }
     
     public int nivelAlcoholSangre()
     {
         return cantidadAlcoholSangre;
+    }
+    
+    public void responderPreguntas(String pregunta)
+    {
+        if(cantidadAlcoholSangre > nivelMaximo || pregunta.contains(nombreBebedor))
+        {
+            System.out.println(pregunta.toUpperCase()+ "!!!!!!!!!");
+        }
+        else
+        {
+            int caracteres = pregunta.length();
+            if(caracteres % 2 == 0)
+            {
+                System.out.println("Si");
+            }
+            else
+            {
+                System.out.println("No");
+            }
+        }
     }
 }
